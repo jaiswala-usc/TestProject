58469d44d277a2cd7b76781af77e13969dacdd5c
Abhishek Jaiswal <jaiswala@usc.edu>
2019-03-13 09:52:48 -0700

    first commit

---
+++ Project1/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace Project1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(2 == 2);
+        }
+    }
+}
